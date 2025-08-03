b13b5acca3ea137b446df7acb5bb97bafe932a2a
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2025-08-03 13:36:33 -0700

    Update Debt model with additional fields
    * Added "category" field to clarify type
    * Added "target" field to highlight current focus payment

--- models/Debt.ts
+++ models/Debt.ts
@@ -1,8 +1,10 @@
 export interface Debt {
   id: string;
   name: string;
+  type: string;
   initialValue: number;
   balance: number;
   minPayment: number;
+  target: boolean;
   interest?: number;
 }
