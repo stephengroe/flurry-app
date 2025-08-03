247e98e97976b38ecc2680ec40e6c7a7700ad17c
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2025-08-03 10:11:26 -0700

    Add basic test functionality for asyncLocalStorage
    * Add button to save and display local data
    * Add integration with useState
    * Import button type from gluestack-ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,36 @@
+import { Button, ButtonText } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
 import { Text } from "@/components/ui/text";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Link } from "expo-router";
+import React, { useState } from "react";
 import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const [user, setUser] = useState("anonymous user");
+
+  const storeData = async (value: string) => {
+    try {
+      await AsyncStorage.setItem("user", value);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem("user");
+      if (value !== null) {
+        setUser(value);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <SafeAreaView>
       <View>
@@ -22,6 +46,18 @@ export default function Index() {
             gamification (streaks + badges).
           </Text>
         </Card>
+        <Button
+          size="lg"
+          variant="solid"
+          action="primary"
+          onPress={() => storeData("Alex")}
+        >
+          <ButtonText>Save Data</ButtonText>
+        </Button>
+        <Button size="lg" variant="solid" action="primary" onPress={getData}>
+          <ButtonText>Retrieve data</ButtonText>
+        </Button>
+        <Text>{user}</Text>
       </View>
     </SafeAreaView>
   );
