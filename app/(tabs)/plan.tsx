be55f6f0f5655753704cd2a3dd70ef6c88b61417
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2025-08-02 17:25:51 -0700

    Set up padding for iOS safety area
    * Add react-native-safe-area-context
    * Add SafeAreaView around major pages

--- app/(tabs)/plan.tsx
+++ app/(tabs)/plan.tsx
@@ -2,16 +2,21 @@ import { Card } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
 import { Text } from "@/components/ui/text";
 import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
 
 export default function Plan() {
   return (
-    <View>
-      <Card size="lg" variant="elevated"  className="m-3">
-        <Text size="lg" className="mb-1">
-          Plan
-        </Text>
-        <Heading size="lg">April 2027</Heading>
-      </Card>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Card size="lg" variant="elevated"  className="m-3">
+          <Text size="lg" className="mb-1">
+            Plan
+          </Text>
+          <Heading size="lg">April 2027</Heading>
+        </Card>
+      </View>
+    </SafeAreaView>
+
   );
 }
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -2,16 +2,19 @@ import { Card } from "@/components/ui/card";
 import { Heading } from "@/components/ui/heading";
 import { Text } from "@/components/ui/text";
 import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Settings() {
   return (
-    <View>
-      <Card size="lg" variant="elevated"  className="m-3">
-        <Text size="lg" className="mb-1">
-          Settings
-        </Text>
-        <Heading size="lg">April 2027</Heading>
-      </Card>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Card size="lg" variant="elevated"  className="m-3">
+          <Text size="lg" className="mb-1">
+            Settings
+          </Text>
+          <Heading size="lg">April 2027</Heading>
+        </Card>
+      </View>
+    </SafeAreaView>
   );
 }
