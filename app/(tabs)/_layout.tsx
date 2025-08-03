8f9599dcf34a50abacc29e9bb118e524c6f4a14e
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2025-08-02 17:17:37 -0700

    Move home to index
    * Originally had /index redirect to /home
    * On second thought, makes more sense just to make /index
      the equivalent of /home and give it an alias of 'home'

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,8 +3,8 @@ import { Tabs } from 'expo-router';
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ href: null, headerShown: false }}  />
-      <Tabs.Screen name="home" options={{ title: "Home", headerShown: false }} />
+
+      <Tabs.Screen name="index" options={{ title: "Home", headerShown: false }} />
       <Tabs.Screen name="activity" options={{ title: "Activity", headerShown: false  }} />
       <Tabs.Screen name="plan" options={{ title: "Plan", headerShown: false  }} />
       <Tabs.Screen name="settings" options={{ title: "Settings", headerShown: false  }} />
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,17 @@
-import { Redirect } from "expo-router";
+import { Card } from "@/components/ui/card";
+import { Heading } from "@/components/ui/heading";
+import { Text } from "@/components/ui/text";
+import { View } from "react-native";
 
 export default function Index() {
-  return <Redirect href="/home" />;
+  return (
+    <View>
+      <Card size="lg" variant="elevated"  className="m-3">
+        <Text size="lg" className="mb-1">
+          Home
+        </Text>
+        <Heading size="lg">April 2027</Heading>
+      </Card>
+    </View>
+  );
 }
