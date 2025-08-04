cfe3f4da7a94d87dacbd68667d65beaf7d67831b
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2025-08-04 16:06:03 -0700

    Add Fab element to link to create new payment

--- app/(tabs)/payments.tsx
+++ app/(tabs)/payments.tsx
@@ -1,9 +1,12 @@
 import { PaymentCard } from "@/components/payment-card";
+import { Fab, FabLabel } from "@/components/ui/fab";
 import { Heading } from "@/components/ui/heading";
 import { VStack } from "@/components/ui/vstack";
 import { useDebtStore } from "@/stores/useDebtStore";
 import { usePaymentStore } from "@/stores/usePaymentStore";
 import { Debt } from "@/types/Debt";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { router } from "expo-router";
 import { useEffect } from "react";
 import { ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -11,6 +14,12 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function Payments() {
   const { payments, loadPayments } = usePaymentStore();
   const { debts, loadDebts } = useDebtStore();
+
+  const createPayment = () => {
+    router.navigate({
+      pathname: "../payment-modal",
+    });
+  };
 
   useEffect(() => {
     loadPayments();
@@ -40,6 +49,19 @@ export default function Payments() {
           )}
         </VStack>
       </ScrollView>
+      <Fab
+        size="lg"
+        placement="bottom right"
+        isHovered={false}
+        isDisabled={false}
+        isPressed={false}
+        onPress={createPayment}
+        className="bg-white"
+      >
+        <FabLabel className="flex-row justify-center items-center">
+          <Ionicons name="add-outline" size={24} color="grey" />
+        </FabLabel>
+      </Fab>
     </SafeAreaView>
   );
 }
